51fba4e2fc409e40577c4989be75294cbc5d1d7c
Ümithan Güldemir <guldemirumithan@gmail.com>
2022-08-15 16:39:57 +0300

    Initial Commit

---
+++ whichNationIsIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whichNationIsIt
+//
+//  Created by Umithan  on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
